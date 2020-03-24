3776859bce45cb9f5cc73149b093ebe93a1c4b15
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-24 03:58:19 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -10,11 +10,7 @@ end
 
 def turtle_traits(turtles)
   turtles.map do |turtle|
-    binding.pry
-    turtle[:traits].each do |traits|
-      binding.pry
-      puts traits
-      end
+    puts turtle[:traits]
     end
   turtles
 end
